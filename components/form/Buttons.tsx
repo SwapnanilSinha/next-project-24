a9ac40e62d4276deaaaf095e453a847389ae6e4c
Swapnanil Sinha <djkrunoze@gmail.com>
2024-09-09 20:07:12 +0530

    Till #72

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+'use client';
 import { ReloadIcon } from '@radix-ui/react-icons';
 import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
@@ -9,12 +8,26 @@ type SubmitButtonProps = {
     text?: string;
 };
 
-export function SubmitButton({ className = '', text = 'submit' }: SubmitButtonProps) {
+export function SubmitButton({
+    className = '',
+    text = 'submit',
+}: SubmitButtonProps) {
     const { pending } = useFormStatus();
-    return <Button type='submit' disabled={pending} className='{`capitalize ${className}`}' size='lg'>
-        {pending ? <>
-            <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
-            Please wait...
-        </> : text}
-    </Button>
-};
+    return (
+        <Button
+            type='submit'
+            disabled={pending}
+            className={`capitalize ${className}`}
+            size='lg'
+        >
+            {pending ? (
+                <>
+                    <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
+                    Please wait...
+                </>
+            ) : (
+                text
+            )}
+        </Button>
+    );
+}
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,15 +1,36 @@
-'use server';
+'use server'
 
 import { profileSchema } from './schemas';
+import db from './db';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export const createProfileAction = async (prevState: any, formData: FormData) => {
     try {
+        const user = await currentUser();
+        if(!user) throw new Error('Not logged in.');
+
         const rawData = Object.fromEntries(formData);
-        const validatedFields = profileSchema.parse(rawData);
-        console.log(validatedFields);
+        // const validatedFields = profileSchema.parse(rawData);
+        await db.profile.create({
+            data: {
+                clerkId: user.id,
+                email: user.emailAddresses[0].emailAddress,
+                profileImage: user.imageUrl ?? '',
+                // ...validatedFields,
+                firstName: 'sample', lastName: '1234', username:'sample'
+            }
+        });
+        await clerkClient.users.updateUserMetadata(user.id, {
+            privateMetadata: {
+                hasProfile: true,
+            },
+        });
         return { message: 'Profile was created.' };
-    } catch (error) {
-        console.log(error);
-        return { message: 'There was an error.' };
+    } catch (err) {
+        console.log(err);
+        return { message: err instanceof Error ? err.message : 'There was an error.' };
     }
+    redirect('/');
 };
