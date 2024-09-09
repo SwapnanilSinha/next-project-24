def5c77cc5870e949ca4114eaa55b5042a39016d
Swapnanil Sinha <djkrunoze@gmail.com>
2024-09-09 09:33:38 +0530

    Till #65
    Before ZOD Library

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,29 @@
+import { SubmitButton } from "@/components/form/Buttons";
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
+
+const createProfileAction = async (prevState:any, formData: FormData) => {
+  'use server'
+  const firstName = formData.get('firstName') as string;
+  return {message:'Profile Created'};
+};
+
+function CreateProfilePage() {
+  return (
+    <section>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>New User</h1>
+      <div className='border p-8 rounded-md max-w'>
+        <FormContainer action={createProfileAction}>
+          <div className="grid md:grid-cols-2 gap-4 mt-4">
+          <FormInput type='text' name='firstName' label='First Name' />
+          <FormInput type='text' name='lastName' label='Last Name' />
+          <FormInput type='text' name='userame' label='Username' />
+          </div>
+          <SubmitButton text='Create Profile' className='mt-8' />
+        </FormContainer>
+      </div>
+    </section>
+  )
+}
+
+export default CreateProfilePage
